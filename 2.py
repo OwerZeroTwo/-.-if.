462098b526a4e956e4cf39a3edc4f909826ecb3e
OwerZeroTwo <161704626+OwerZeroTwo@users.noreply.github.com>
2024-02-29 12:40:24 +0300

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,10 @@
+a, b = 10, 5
+
+if a > b:
+    print(a > b)
+if a > b and a > 0:
+    print('Успех')
+if (a > b) and (a > 0 or b < 1000):
+    print('Успех')
+if 5 < b and b > 10:
+    print('Успех')
